97c9f86d99adbecb545a6d1daf6a764206635754
Joshua Stephenson <josh@brightmediums.com>
2020-07-15 11:55:06 -0400

    Plot 3 Energy sub metering over DoW with legend

---
+++ plot3.R
@@ -0,0 +1,34 @@
+library(lubridate)
+library(dplyr)
+filename <- "./household_power_consumption.txt"
+start <- 66636
+end <- 69516
+# Only read in lines that correspond to dates: 01/02/2020 - 02/02/2020 (format: DD/MM/YYYY)
+hpc <- read.table(filename, sep = ";", header = TRUE, skip = start, nrows = (end - start), na.strings = "?")
+names(hpc) <- c("Date", 
+                "Time", 
+                "GlobalActivePower", 
+                "GlobalReactivePower", 
+                "Voltage",
+                "GlobalIntensity",
+                "SubMetering1",
+                "SubMetering2",
+                "SubMetering3"
+)
+hpc <- hpc %>%
+        mutate(DateTime = dmy_hms(paste(Date, Time))) %>%
+        select(DateTime, SubMetering1, SubMetering2, SubMetering3)
+
+# Open a PNG device where we will draw the plot
+png(file = "plot3.png")
+
+# Reference images included in repository have transparent bg
+par(bg = "transparent")
+with(hpc, plot(SubMetering1 ~ DateTime, col = c("black", "red", "blue"), type="n", xlab = "", ylab = "Energy sub metering"))
+with(hpc, lines(SubMetering1 ~ DateTime, col = "black"))
+with(hpc, lines(SubMetering2 ~ DateTime, col = "red"))
+with(hpc, lines(SubMetering3 ~ DateTime, col = "blue"))
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# Close the PNG device and save to disk
+dev.off()
