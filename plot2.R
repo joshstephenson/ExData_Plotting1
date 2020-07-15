d2a9d362a2e8d5cfb475c26139daffdadb23d8a5
Joshua Stephenson <josh@brightmediums.com>
2020-07-15 11:54:45 -0400

    Plot 2 line plot of GAP over DoW

---
+++ plot2.R
@@ -0,0 +1,34 @@
+library(lubridate)
+library(dplyr)
+filename <- "./household_power_consumption.txt"
+start <- 66636
+end <- 69516
+# Only read in lines that correspond to dates: 01/02/2020 - 02/02/2020 (format: DD/MM/YYYY)
+hpc <- read.table(filename, sep = ";", header = TRUE, skip = start, nrows = (end - start), na.strings = "?")
+names(hpc) <- c("Date", 
+                "Time", 
+                "GlobalActivePower", 
+                "GlobalReactivePower", 
+                "Voltage",
+                "GlobalIntensity",
+                "SubMetering1",
+                "SubMetering2",
+                "SubMetering3"
+)
+
+# Parse the dates to DateTime objects and Select only the appropriate rows
+hpc <- hpc %>%
+        mutate(DateTime = dmy_hms(paste(Date, Time))) %>%
+        select(DateTime, GlobalActivePower)
+
+# Open a PNG device where we will draw the plot
+png(file = "plot2.png")
+
+# Reference images included in repository have transparent bg
+par(bg = "transparent")
+
+plot(GlobalActivePower ~ DateTime, data = hpc, type = "n", xlab = "", ylab = "Global Active Power (Kilowatts)")
+lines(GlobalActivePower ~ DateTime, data = hpc)
+
+# Close the PNG device and save to disk
+dev.off()
